3dfb32667b4a919f041f1b0586f7ea157ec16a7f
sanctionedpranav <pranav9728@gmail.com>
2025-10-06 21:54:35 +0530

    java cli commands to practice

---
+++ CLIAdd.java
@@ -0,0 +1,18 @@
+
+// public class CLIGreet {
+//     public static void main(String args[]) {
+//         System.out.println(args[0] + " " + args[1] + " " + args[2]);
+//   }
+// }
+public class CLIAdd {
+
+    public static void main(String args[]) {
+        int sum = 0;
+
+        for (int i = 0; i < args.length; i++) {
+            sum = sum + Integer.parseInt(args[i]);
+        }
+        System.out.println(sum);
+    }
+
+}
