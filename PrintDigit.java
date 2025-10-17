f9eeed488614d8008e175a53f426d4855538e107
sanctionedpranav <pranav9728@gmail.com>
2025-10-17 14:07:26 +0530

    print entered digits vertically done also added the scanner.close() in primeNumber.java

---
+++ PrintDigit.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class PrintDigit {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter your number to print vertically:");
+    int num = scanner.nextInt();
+    int copy = num;
+    int count = 0;
+
+    while (num != 0) {
+      num = num / 10;
+      count++;
+    }
+
+    num = copy;
+    int power = (int) Math.pow(10, count - 1);
+
+    while (num != 0) {
+      int startDigit = num / power;
+      System.out.println(startDigit);
+
+      num = num % power;
+      power = power / 10;
+    }
+
+    scanner.close();
+  }
+}
