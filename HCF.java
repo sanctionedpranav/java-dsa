f83707f41fbc2638486d854898538a1ed79b58d2
sanctionedpranav <pranav9728@gmail.com>
2025-10-27 23:52:26 +0530

    Factorial, HCF, Power of a number DSA questions

---
+++ HCF.java
@@ -0,0 +1,14 @@
+public class HCF {
+  public static void main(String[] args) {
+    int num1 = 155667;
+    int num2 = 54321;
+    int HCF = 0;
+
+    for (int i = 1; i <= Math.min(num1, num2); i++) {
+      if (num1 % i == 0 && num2 % i == 0) {
+        HCF = i;
+      }
+    }
+    System.out.println(HCF);
+  }
+}
---
+++ PowerOfNumber.java
@@ -0,0 +1,13 @@
+public class PowerOfNumber {
+  public static void main(String[] args) {
+    int num = 5;
+    int copy = num;
+    int power = 6;
+
+    for (int i = 1; i <= power - 1; i++) {
+      num = num * copy;
+    }
+    System.out.println(num);
+
+  }
+}
