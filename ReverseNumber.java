9817d0ac18f1b0fda900e26b747bf9b5dd61b2e1
sanctionedpranav <pranav9728@gmail.com>
2025-10-28 12:44:41 +0530

    reverse number

---
+++ ReverseNumber.java
@@ -0,0 +1,14 @@
+public class ReverseNumber {
+  public static void main(String[] args) {
+    int num = 123;
+    int reverseNumber = 0;
+
+    while (num != 0) {
+      int lastDigit = num % 10;
+      reverseNumber = lastDigit + reverseNumber * 10;
+      num = num / 10;
+    }
+
+    System.out.println(reverseNumber);
+  }
+}
