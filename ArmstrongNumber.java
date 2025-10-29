1adaa3888f3de0cd211215d8ce11c5091f975d2a
sanctionedpranav <pranav9728@gmail.com>
2025-10-29 18:28:48 +0530

    armstrong number

---
+++ ArmstrongNumber.java
@@ -0,0 +1,30 @@
+public class ArmstrongNumber {
+  public static void main(String[] args) {
+    int num = 153;
+    int dupNumber = num;
+    int count = 0;
+    int armstrongNumber = 0;
+
+    while (num != 0) {
+      num = num / 10;
+      count++;
+    }
+
+    num = dupNumber;
+
+    while (num != 0) {
+      int lastDigit = num % 10;
+      armstrongNumber = armstrongNumber + (int) Math.pow(lastDigit, count);
+      num = num / 10;
+    }
+
+    if (dupNumber == armstrongNumber) {
+      System.out.println("Armstrong Number");
+    } else {
+      System.out.println("Not an Armstrong Number" + num);
+    }
+
+    System.out.println("ArmStrong No: " + armstrongNumber);
+
+  }
+}
