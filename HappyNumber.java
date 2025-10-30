908e6470a1c02edcfce2c6217224a6f1a26d6407
sanctionedpranav <pranav9728@gmail.com>
2025-10-30 21:57:39 +0530

    happy number, neon number

---
+++ HappyNumber.java
@@ -0,0 +1,25 @@
+public class HappyNumber {
+  public static void main(String[] args) {
+    int num = 2;
+    int sum = 0;
+
+    while (num != 1) {
+      while (num != 0) {
+        int lastDigit = num % 10;
+        sum = sum + lastDigit * lastDigit;
+        num = num / 10;
+      }
+      num = sum;
+      sum = 0;
+    }
+
+    if(num == 1){
+      System.out.println("Happy Number");
+    } else {
+      System.out.println("Not a Happy Number");
+    }
+
+    System.out.println(num);
+
+  }
+}
---
+++ NeonNumber.java
@@ -0,0 +1,21 @@
+public class NeonNumber {
+  public static void main(String[] args) {
+    int num = 81;
+    int sumNumber = 0;
+    int sqNumber = 0;
+
+    while (num != 0) {
+      int lastDigit = num % 10;
+      sumNumber += lastDigit;
+      sqNumber = sumNumber * sumNumber;
+      num /= 10;
+    }
+
+    if(sumNumber % sqNumber == 0){
+      System.out.println("Neon Number");
+    } else{
+      System.out.println("Not a Neon Number");
+    }
+
+  }
+}
