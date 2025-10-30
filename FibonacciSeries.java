9ce1996f9db9829ee3b6a1b52a001089f6312b6d
sanctionedpranav <pranav9728@gmail.com>
2025-10-30 13:18:17 +0530

    fibonacci series and palindrom number

---
+++ FibonacciSeries.java
@@ -0,0 +1,18 @@
+public class FibonacciSeries {
+  public static void main(String[] args) {
+    int num = 10;
+    int firstNum = 0;
+    int secondNum = 1;
+
+    System.out.print(firstNum+ "" + secondNum);
+
+    for (int i = 2; i < num; i++) {
+      int thirdNum = firstNum + secondNum;
+      System.out.print(thirdNum);
+
+      firstNum = secondNum;
+      secondNum = thirdNum;
+    }
+
+  }
+}
