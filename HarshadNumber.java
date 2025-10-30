b00a75f49219ea16b412ec0040e74de2b42a96ee
sanctionedpranav <pranav9728@gmail.com>
2025-10-30 13:49:41 +0530

    Harshad number

---
+++ HarshadNumber.java
@@ -0,0 +1,22 @@
+public class HarshadNumber {
+  public static void main(String[] args) {
+    int num = 17;
+    int originalNum = num;
+    int sum = 0;
+
+    while (num != 0) {
+      int lastDigit = num % 10;
+      sum = sum + lastDigit;
+      num /= 10;
+    }
+
+    if (originalNum % sum == 0) {
+      System.out.println("Harshad Number");
+    } else {
+      System.out.println("Not a Harshad Number");
+
+    }
+
+    System.out.println(sum);
+  }
+}
