17aa473b23b7a207d5f138d2595ce2c77faec701
sanctionedpranav <pranav9728@gmail.com>
2025-10-31 13:00:43 +0530

    Automorphic number

---
+++ AutomorphicNumber.java
@@ -0,0 +1,17 @@
+public class AutomorphicNumber {
+  public static void main(String[] args) {
+    int num = 6;
+    int square = num * num;
+
+    int lastDigit = square % 10;
+
+    if (lastDigit == num) {
+      System.out.println("Automorphic Number");
+    } else {
+      System.out.println("Not an Automorphic Number");
+    }
+
+    System.out.println(num);
+    System.out.println(lastDigit);
+  }
+}
