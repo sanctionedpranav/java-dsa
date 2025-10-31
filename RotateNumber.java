078a2ffc3fa119bf5878257752f3e635a8bb388f
sanctionedpranav <pranav9728@gmail.com>
2025-10-31 18:29:18 +0530

    Rotate Number

---
+++ RotateNumber.java
@@ -0,0 +1,27 @@
+public class RotateNumber {
+  public static void main(String[] args) {
+    int num = 12345;
+    int originalNum = num;
+    int rotation = 1;
+    int left = 0;
+    int right = 0;
+    int count = 0;
+
+    while (num != 0) {
+      num /= 10;
+      count++;
+    }
+
+    int lastDigits = originalNum % (int) Math.pow(10, rotation);
+    right += lastDigits;
+    left = originalNum / (int) Math.pow(10, rotation);
+
+    right *= (int) Math.pow(10, count - rotation);
+
+    num = left + right;
+
+    System.out.println(lastDigits);
+    System.out.println(num);
+
+  }
+}
