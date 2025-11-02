56aee7e0beef9c2826343d454bf0af47ff1c124e
sanctionedpranav <pranav9728@gmail.com>
2025-11-02 11:53:31 +0530

    perfect number

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,11 +1,12 @@
 public class PerfectNumber {
   public static void main(String[] args) {
-    int num = 6;
+    int num = 28;
     int sum = 0;
 
     for (int i = 1; i <= num / 2; i++) {
       if (num % i == 0) {
         sum += i;
+        System.out.println(i);
       }
     }
 
