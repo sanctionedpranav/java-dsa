5aa8bd9ca574ab7566051bd43a5f56afb935544f
sanctionedpranav <pranav9728@gmail.com>
2025-11-03 11:55:29 +0530

    prime number

---
+++ recursion/PrimeNumber.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+public class PrimeNumber {
+  static void isPrimeNumber(int num, int divisor) {
+
+    if (num < 2) {
+      System.out.println("Not prime number");
+      return;
+    }
+    if ((divisor * divisor) > num) {
+      System.out.println("prime number");
+      return;
+    }
+    if (num % divisor == 0){
+      System.out.println("Not prime number");
+      return;
+    }
+
+      isPrimeNumber(num, divisor + 1);
+  }
+
+  public static void main(String[] args) {
+    int num = 2;
+    int divisor = 2;
+
+    isPrimeNumber(num, divisor);
+  }
+}
