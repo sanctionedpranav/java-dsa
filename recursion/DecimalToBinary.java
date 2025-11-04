52feaf6d2acc1af42ff02a5cfb5cc874971b3834
sanctionedpranav <pranav9728@gmail.com>
2025-11-04 13:16:47 +0530

    deciml to binary in recursion

---
+++ recursion/DecimalToBinary.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class DecimalToBinary {
+  static void convertDecimalToBinary(int num, String result) {
+    if (num == 0) {
+      System.out.println(result);
+      return;
+    }
+
+    convertDecimalToBinary(num / 2, num % 2 + result);
+  }
+
+  public static void main(String[] args) {
+    int num = 18;
+    String result = "";
+
+    convertDecimalToBinary(num, result);
+  }
+}
