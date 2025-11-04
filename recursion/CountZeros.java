b0eef7eac1c8a76eb521b894c1facfcded505c39
sanctionedpranav <pranav9728@gmail.com>
2025-11-04 12:35:44 +0530

    count zero in the number

---
+++ recursion/CountZeros.java
@@ -0,0 +1,31 @@
+package Recursion;
+
+public class CountZeros {
+  static int countZeros(int num) {
+    if (num == 0) {
+      return 1;
+    }
+
+    if (num < 10 && num != 0) {
+      return 0;
+    }
+
+    int lastDigit = num % 10;
+    int count = 0;
+
+    if (lastDigit == 0) {
+      count++;
+    }
+
+    return count + countZeros(num / 10);
+
+  }
+
+  public static void main(String[] args) {
+    int num = 100000;
+
+    int result = countZeros(num);
+    System.out.println(result);
+
+  }
+}
