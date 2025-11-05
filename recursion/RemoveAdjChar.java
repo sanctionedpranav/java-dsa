03a0526931bed7a09ad85b50702ec635a52b13f3
sanctionedpranav <pranav9728@gmail.com>
2025-11-06 02:51:36 +0530

    adjacent character removal logic

---
+++ recursion/RemoveAdjChar.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+public class RemoveAdjChar {
+
+  static String removeAdjChar(String str) {
+    if (str.length() <= 1) {
+      ret1`urn "";
+    }
+
+    String updatedString = removeAdjChar(str.substring(1));
+
+    if (str.charAt(0) == updatedString.charAt(0)) {
+      return updatedString;
+    } else {
+      return str.charAt(0) + updatedString;
+    }
+
+  }
+
+  public static void main(String[] args) {
+    String str = "aabbccddc";
+
+    String result = removeAdjChar(str);
+    System.out.println(result);
+  }
+}
