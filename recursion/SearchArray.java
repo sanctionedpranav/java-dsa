0b8421b94de28e3896103de2ac141e2c397e1cec
sanctionedpranav <pranav9728@gmail.com>
2025-11-06 04:50:15 +0530

    Search a number in array

---
+++ recursion/SearchArray.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class SearchArray {
+  static int searchArray(int[] arr, int index, int searchNum) {
+    if (arr.length == index) {
+      return 0;
+    }
+
+    int count = 0;
+
+    if (arr[index] == searchNum) {
+      count++;
+    }
+
+    return count + searchArray(arr, index + 1, searchNum);
+  }
+
+  public static void main(String[] args) {
+    int arr[] = { 10, 20, 99, 20, 76, 20, 50, 20 };
+    int searchNum = 20;
+
+    System.out.println(searchArray(arr, 0, searchNum));
+  }
+}
