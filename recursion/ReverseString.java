2d8d0d29d57219bc65996354a19ef51ab488586c
sanctionedpranav <pranav9728@gmail.com>
2025-11-06 01:57:00 +0530

    reversed string logic

---
+++ recursion/ReverseString.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class ReverseString {
+  static String reverseStr(String str) {
+    if (str.length() == 0) {
+      return "";
+    }
+
+    String lastChar = reverseStr(str.substring(0, str.length() - 1));
+    String reversedString = str.charAt(str.length() - 1) + lastChar;
+
+    return reversedString;
+  }
+
+  public static void main(String[] args) {
+    String str = "Pranav";
+
+    String reversedStr = reverseStr(str);
+    System.out.println(reversedStr);
+  }
+}
