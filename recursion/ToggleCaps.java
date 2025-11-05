5ecf94a37183814ca26143a5568aa8cc8fe8effd
sanctionedpranav <pranav9728@gmail.com>
2025-11-06 03:15:36 +0530

    Toggle the caps... means capital letters will get small and small characters will capitalize

---
+++ recursion/ToggleCaps.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+public class ToggleCaps {
+  static String toggleCaps(String str) {
+    if (str.length() == 0) {
+      return "";
+    }
+
+    String smallProblemString = toggleCaps(str.substring(1));
+
+    char firstChar = str.charAt(0);
+
+    if (firstChar >= 65 && firstChar <= 90) {
+      firstChar = (char) (firstChar + 32);
+    } else {
+      firstChar = (char) (firstChar - 32);
+    }
+
+    return firstChar + smallProblemString;
+  }
+
+  public static void main(String[] args) {
+    String str = "aBcDeFgH";
+
+    System.out.println(toggleCaps(str));
+  }
+}
