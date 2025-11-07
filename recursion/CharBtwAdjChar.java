f24f58c7fb9d2381159b40ab51c134e2819221bc
sanctionedpranav <pranav9728@gmail.com>
2025-11-07 20:01:06 +0530

    Star between adjacent character

---
+++ recursion/CharBtwAdjChar.java
@@ -0,0 +1,29 @@
+package Recursion;
+
+public class CharBtwAdjChar {
+  static String charBtwAdjChar(String str) {
+    if (str.length() == 1) {
+      return str;
+    }
+
+    String updatedString = charBtwAdjChar(str.substring(1));
+
+    char firstChar = str.charAt(0);
+    char updatedFirstChar = updatedString.charAt(0);
+
+    if (firstChar == updatedFirstChar) {
+      updatedString = firstChar + "*" + updatedFirstChar;
+    } else{
+      updatedString = firstChar + updatedString;
+    }
+
+    return updatedString;
+  }
+
+  public static void main(String[] args) {
+    String str = "aabbccdd";
+
+    String result = charBtwAdjChar(str);
+    System.out.println(result);
+  }
+}
