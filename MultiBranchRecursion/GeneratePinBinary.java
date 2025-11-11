01a22154963d5324c9e99df08325fe6c052f6867
sanctionedpranav <pranav9728@gmail.com>
2025-11-11 22:57:54 +0530

    Generate pin of input length in binary logic in resursion

---
+++ MultiBranchRecursion/GeneratePinBinary.java
@@ -0,0 +1,20 @@
+package MultiBranchRecursion;
+
+public class GeneratePinBinary {
+
+  static void generatePinBinary(int length, String result) {
+    if (length == 0) {
+      System.out.println(result);
+      return;
+    }
+
+    generatePinBinary(length - 1, result + 0); // Not Pick
+    generatePinBinary(length - 1, result + 1); // Pick
+  }
+
+  public static void main(String[] args) {
+    int length = 3;
+
+    generatePinBinary(length, "");
+  }
+}
