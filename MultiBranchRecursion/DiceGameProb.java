0ab6359e60722865752de5d6a85b888bd2217890
sanctionedpranav <pranav9728@gmail.com>
2025-11-11 22:06:59 +0530

    Dice game problem multu branch rescursion

---
+++ MultiBranchRecursion/DiceGameProb.java
@@ -0,0 +1,22 @@
+package MultiBranchRecursion;
+
+public class DiceGameProb {
+  static void diceGameProb(int start, int end, String result) {
+    if (start == end) {
+      System.out.print(result + ", ");
+      return;
+    }
+    if (start > end) {
+      return;
+    }
+
+    for (int i = 1; i <= 2; i++) {
+      diceGameProb(start + i, end, result + i);
+    }
+
+  }
+
+  public static void main(String[] args) {
+    diceGameProb(0, 10, "");
+  }
+}
