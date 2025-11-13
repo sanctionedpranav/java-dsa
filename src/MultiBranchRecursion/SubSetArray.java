fe48b8e57bc69930a2e83886bc5ad590b85d2115
sanctionedpranav <pranav9728@gmail.com>
2025-11-13 13:33:18 +0530

    Sub sets of an array

--- src/MultiBranchRecursion/SubSetArray.java
+++ src/MultiBranchRecursion/SubSetArray.java
@@ -1,7 +1,19 @@
 package MultiBranchRecursion;
 
 public class SubSetArray {
+  static void subSet(int arr[], int index, String result) {
+    if (index == arr.length) {
+      System.out.println(result);
+      return;
+    }
+
+    subSet(arr, index + 1, result);
+    subSet(arr, index + 1, result + arr[index]);
+  }
+
   public static void main(String[] args) {
-    
+    int arr[] = { 1, 2, 3 };
+
+    subSet(arr, 0, "");
   }
 }
