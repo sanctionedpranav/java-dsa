41471fb5837be0c949902f0a6ddf1a1d07e750fe
sanctionedpranav <pranav9728@gmail.com>
2025-11-14 15:01:09 +0530

    count sub set in the array

---
+++ src/MultiBranchRecursion/CountSubSetArray.java
@@ -0,0 +1,20 @@
+package MultiBranchRecursion;
+
+public class CountSubSetArray {
+  static void countSubSet(int arr[], int index, String result) {
+
+    for (int i = index; i < arr.length; i++) {
+      System.out.println(result + arr[i]);
+      countSubSet(arr, i + 1, result + arr[i]);
+    }
+
+    // return countSubSet(arr, index + 1) + countSubSet(arr, index + 1);
+
+  }
+
+  public static void main(String[] args) {
+    int arr[] = { 1, 2, 3 };
+
+    countSubSet(arr, 0, "");
+  }
+}
